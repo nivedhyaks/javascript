552e130d45adb0952580bf4dbc92a31e87a57ea6
nivedhyaks <nivedhyaks2003@gmail.com>
2024-08-07 19:32:34 +0530

    version 1

---
+++ main.js
@@ -0,0 +1,14 @@
+//display content
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+
+  function calculate() {
+    var display = document.getElementById('display').value;
+    var result = eval(display);
+    document.getElementById('display').value = result;
+  }
